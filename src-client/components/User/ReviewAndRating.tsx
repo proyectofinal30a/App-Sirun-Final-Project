b7c6d3901873f93c223cc117c003b5453d05e4b5
mailin <mailinadaromdp@gmail.com>
2022-11-20 14:23:07 -0300

    Boton delete

--- src-client/components/User/ReviewAndRating.tsx
+++ src-client/components/User/ReviewAndRating.tsx
@@ -31,38 +31,38 @@ const ReviewAndRating = () => {
   return (
     <div className={styles.reviews__container}>
       <h1 className={styles.reviews__title}>My reviews</h1>
-
       {evaluations?.map((elem) => {
         const { product } = elem;
         const { image } = product.image[0];
         const {id} = elem;
-        
-
         return (
-          <div key={elem.id} className={styles.reviews__review_container}>
-            <div className={styles.reviews__img_container}>
-              <Image
-                src={image}
-                width="300"
-                height="300"
-                alt={product.name}
-                className={styles.reviews__img}
-              />
-            </div>
-
-
-            <div className={styles.reviews__info_container}>
-
-
-              <div className={styles.reviews__info}>
-                <p className={styles.reviews__name}>
-                  {product.name.toLowerCase()}
-                </p>
-
-                <Link href={`/productDetail/${product.id}`} className={styles.reviews__details}>
-                  View product details
-                </Link>
+          <div className={styles.review__container} key={id}>
+            <div key={elem.id} className={styles.reviews__review_container}>
+              <div className={styles.reviews__img_container}>
+                <Image
+                  src={image}
+                  width="300"
+                  height="300"
+                  alt={product.name}
+                  className={styles.reviews__img}
+                />
               </div>
+
+              <div className={styles.reviews__info_container}>
+                    
+              
+                <div className={styles.reviews__info}>
+             <div className={styles.first__line_container}> 
+                  <p className={styles.reviews__name}>
+                    {product.name.toLowerCase()}
+                  </p>
+                <button className={styles.delete__review}  onClick={() => handleDelete(id, email)}>x</button>
+              </div>
+
+                  <Link href={`/productDetail/${product.id}`} className={styles.reviews__details}>
+                    View product details
+                  </Link>
+                </div>
 
               <p className={styles.reviews__review_message}>
                 <span className={styles.reviews__span}>Review message: </span>
@@ -79,13 +79,9 @@ const ReviewAndRating = () => {
                      : Array(elem.rating).fill(<FaStar className={styles.stars__filled}/>)}
                 </div>
               </div>
-
             </div>
-            
-              <div className={styles.delete__container}>
-                <button className={styles.delete__review}  onClick={() => handleDelete(id, email)}>x</button>
-              </div>
           </div>
+        </div>
         );
       })}
     </div>
