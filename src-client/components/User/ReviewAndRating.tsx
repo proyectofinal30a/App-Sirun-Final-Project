2640365213bec1772f927a741bec72b915c07f14
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2022-11-20 19:06:10 -0300

    cambio de rama a develop

--- src-client/components/User/ReviewAndRating.tsx
+++ src-client/components/User/ReviewAndRating.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Ireducers } from "../../../lib/types";
-import {deleteReview} from "../../redux/slice/user-review/user-review"
-import {getUserDetail} from '../../redux/slice/user-detail-redux/user-redux'
+import { deleteReview } from "../../redux/slice/user-review/user-review"
+import { getUserDetail } from '../../redux/slice/user-detail-redux/user-redux'
 import Link from "next/link";
 import Image from "next/image";
 import { FaStar } from "react-icons/fa";
@@ -11,21 +11,13 @@ import styles from "../../styles/ReviewAndRating.module.css";
 
 const ReviewAndRating = () => {
   const myProfile = useSelector((state: Ireducers) => state.reducerUser.user);
-  const dispatch : Function = useDispatch()
-
-  const {email} = myProfile; 
-
-  
-
-
+  const dispatch: Function = useDispatch()
   if (!myProfile) return <div className={styles.loading}>Loading...</div>;
-
+  const { email } = myProfile;
   const { evaluations } = myProfile;
-
-
   if (!evaluations) return <div className={styles.loading}>Loading...</div>;
 
-  const handleDelete = (id: string, email : string) => { 
+  const handleDelete = (id: string, email: string) => {
     dispatch(deleteReview(id));
     dispatch(getUserDetail(email))
   };
@@ -36,7 +28,7 @@ const ReviewAndRating = () => {
       {evaluations?.map((elem) => {
         const { product } = elem;
         const { image } = product.image[0];
-        const {id} = elem;
+        const { id } = elem;
         return (
           <div className={styles.review__container} key={id}>
             <div key={elem.id} className={styles.reviews__review_container}>
@@ -51,51 +43,52 @@ const ReviewAndRating = () => {
               </div>
 
               <div className={styles.reviews__info_container}>
-                    
-              
+
+
                 <div className={styles.reviews__info}>
-             <div className={styles.first__line_container}> 
-                  <p className={styles.reviews__name}>
-                    {product.name.toLowerCase()}
-                  </p>
-                <button className={styles.delete__review}  onClick={() => handleDelete(id, email)}>x</button>
-              </div>
+                  <div className={styles.first__line_container}>
+                    <p className={styles.reviews__name}>
+                      {product.name.toLowerCase()}
+                    </p>
+                    <button className={styles.delete__review} onClick={() => handleDelete(id, email)}>x</button>
+                  </div>
 
                   <Link href={`/productDetail/${product.id}`} className={styles.reviews__details}>
                     View product details
                   </Link>
                 </div>
 
-              <p className={styles.reviews__review_message}>
-                <span className={styles.reviews__span}>Review message: </span>
-                {elem.review}
-              </p>
+                <p className={styles.reviews__review_message}>
+                  <span className={styles.reviews__span}>Review message: </span>
+                  {elem.review}
+                </p>
 
 
-              <div className={styles.reviews__rating_container}>
-                <span className={styles.reviews__span}>Rating:   </span>
-                <div className={styles.reviews__rating_value_container}>
-                         
-               {Array(5).fill(0).map( 
-                (star: number, index: number) => {
-                  const ratingValue = index + 1;
-                  return (
-                    <div key={index}>
-                      <FaStar
-                        className={
-                          ratingValue <= elem.rating
-                            ? styles.stars__filled
-                            : styles.stars__empthy
-                        } 
-                      />
-                    </div>
-                  ); })}
+                <div className={styles.reviews__rating_container}>
+                  <span className={styles.reviews__span}>Rating:   </span>
+                  <div className={styles.reviews__rating_value_container}>
 
+                    {Array(5).fill(0).map(
+                      (star: number, index: number) => {
+                        const ratingValue = index + 1;
+                        return (
+                          <div key={index}>
+                            <FaStar
+                              className={
+                                ratingValue <= elem.rating
+                                  ? styles.stars__filled
+                                  : styles.stars__empthy
+                              }
+                            />
+                          </div>
+                        );
+                      })}
+
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         );
       })}
     </div>
