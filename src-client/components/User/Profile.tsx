ae752310a1ea84b7755abac766b108d46f861d31
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-20 21:28:53 +0100

    profile updated

--- src-client/components/User/Profile.tsx
+++ src-client/components/User/Profile.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styles from "../../styles/Account.module.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -7,12 +8,13 @@ import { Ireducers } from "../../../lib/types";
 import cloudinaryOrUrl from "../../controllers/detectionOfImage";
 import { postImageServerUsert } from "../../redux/slice/user-detail-redux/user-redux";
 
-export default function Profile(): JSX.Element {
+const Profile = () => {
   const myStateForm = {
     image: "",
     name: "",
     status: true,
   };
+
   const [imageUser, setImageUser] = useState(null);
   const dispatch: Function = useDispatch();
   const [previewForm, setPreviewFrom] = useState(myStateForm);
@@ -21,11 +23,13 @@ export default function Profile(): JSX.Element {
     | React.FormEvent<HTMLFormElement>
     | React.ChangeEvent<HTMLInputElement>
     | React.ChangeEvent<HTMLSelectElement>;
+    
   const myProfide = useSelector((state: Ireducers) => state.reducerUser.user);
-  if (!myProfide) return <div>Loading</div>;
+  if (!myProfide) return <div>Loading...</div>;
   const { name, email, image, direcciones } = myProfide;
 
   const myimage = cloudinaryOrUrl(image, "client");
+
 
   const handleOnFile = (event: any) => {
     const imageFile = event.target.files;
@@ -39,10 +43,12 @@ export default function Profile(): JSX.Element {
     setPreviewFrom({ ...previewForm, image: imgURL });
   };
 
+
   const handleOnchage = (event: any) => {
     const { value } = event.target;
     setPreviewFrom({ ...previewForm, name: value });
   };
+
 
   const handleOnsubmit = async (event: valueForm) => {
     event.preventDefault();
@@ -52,95 +58,91 @@ export default function Profile(): JSX.Element {
       email,
       deleteImage: image,
     };
+    await postImageServerUsert(packFormUserUpdate);
+    setPreviewFrom(myStateForm);
 
-    const handleOnchage = (event: any) => {
-        const { value } = event.target
-        setPreviewFrom({ ...previewForm, name: value })
-    }
-
-    const handleOnsubmit = async (event: valueForm) => {
-        event.preventDefault()
-        const packFormUserUpdate = {
-            name: previewForm.name,
-            newImage: imageUser,
-            email,
-            deleteImage: image
-        }
-        await postImageServerUsert(packFormUserUpdate)
-        setPreviewFrom(myStateForm)
-
-        dispatch(getUserDetail(email))
-
-    }
+    dispatch(getUserDetail(email));
+  };
 
 
-    const myImage: any = previewForm.image || myimage
-    const myName: string = previewForm.name || name
-    
-    const handleOnclikSwich = () => {
-        setPreviewFrom({
-            image: '',
-            name: '',
-            status: !previewForm.status
-        })
-    }
+  const myImage: any = previewForm.image || myimage;
+  const myName: string = previewForm.name || name;
 
-    const myForm = (
-        <form className={styles.form__container} onSubmit={handleOnsubmit} >
-            <input className={styles.form__input} type="text" placeholder=" My Name" value={previewForm.name} onChange={handleOnchage} />
-            <input
-                className={styles.btn__img} 
-                type="file"
-                accept=".jpg , .png , .jpeg"
-                onChange={handleOnFile}
-                name="image"
-                required
-            />
-            <div className={styles.election__btn}>
-                <button   className={styles.btn} onClick={handleOnclikSwich}>Revert</button>
-                <input   className={styles.btn__submit} type="submit" />
-            </div>
-        </form>
-    )
+  const handleOnclikSwich = () => {
+    setPreviewFrom({
+      image: "",
+      name: "",
+      status: !previewForm.status,
+    });
+  };
 
 
+  const myForm = (
+    <form className={styles.form__container} onSubmit={handleOnsubmit}>
+      <input
+        className={styles.form__input}
+        type="text"
+        placeholder=" My Name"
+        value={previewForm.name}
+        onChange={handleOnchage}
+      />
+      <input
+        className={styles.btn__img}
+        type="file"
+        accept=".jpg , .png , .jpeg"
+        onChange={handleOnFile}
+        name="image"
+        required
+      />
+      <div className={styles.election__btn}>
+        <button className={styles.btn} onClick={handleOnclikSwich}>
+          Revert
+        </button>
+        <input className={styles.btn__submit} type="submit" />
+      </div>
+    </form>
+  );
 
-    const myButtonSwith = previewForm.status ?
-        <button  className={styles.switch__btn} onClick={handleOnclikSwich}>Profile Edition</button> :
-        myForm;
+  const myButtonSwith = previewForm.status ? (
+    <button className={styles.switch__btn} onClick={handleOnclikSwich}>
+      Profile Edition
+    </button>
+  ) : (
+    myForm
+  );
 
-    const myAdress = direcciones?.map((ele, index :number) => (
-        <div className={styles.adress} key={index}>
-            <p>Direccion {index + 1}:  {ele.dir} </p>
-        </div>
-    ))
-    return (
+  const myAdress = direcciones?.map((ele, index: number) => (
+    <div className={styles.adress} key={index}>
+      <p>
+        Direccion {index + 1}: {ele.dir}{" "}
+      </p>
+    </div>
+  ));
 
-        <div>
-            <div className={styles.profile__container}>
+  
+  return (
+    <div>
+      <div className={styles.profile__container}>
+        <Image
+          src={myImage || "https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif"}
+          width="200"
+          height="200"
+          alt={name}
+          className={styles.avatar__image}
+        />
+        <h1 className={styles.profile__title}>{myName.toUpperCase()}</h1>
+      </div>
 
-                <Image
-                    src={myImage || " "}
-                    width='100'
-                    height='100'
-                    alt={name}
-                    className={styles.avatar__image}
-                    />
-                    <h1 className={styles.profile__title}>{myName.toUpperCase()}</h1>
-            </div>
-                    
-            <div className={styles.adress__container}>
-                <p>Email: {email}</p>
-                {myAdress}
-            </div>
-            
-            <div className={styles.btn__aling}>
-                {myButtonSwith}
-            </div>  
-        </div>
-        <div>{myButtonSwith}</div>
+      <div className={styles.adress__container}>
+        <p>Email: {email}</p>
         {myAdress}
       </div>
+
+      <div className={styles.btn__aling}>{myButtonSwith}</div>
+
+      {myAdress}
     </div>
   );
-}
+};
+
+export default Profile;
