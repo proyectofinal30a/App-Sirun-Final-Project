5bd781ca5f254309d22105ecadac89e8bc31a3c3
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-21 15:46:26 +0100

    Mejoras en admin components

--- src-client/components/Administration/AdminManageProducts.tsx
+++ src-client/components/Administration/AdminManageProducts.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../styles/AdminManageProducts.module.css";
 
 
 const AdminManageProducts = () => {
   return (
     <div className={styles.products_form__container}>
-        <h1 className={styles.products_form__title}>Administration product managing form coming soon</h1>
+        <h1 className={styles.products_form__title}>Administration product managing form</h1>
+        <Image 
+          src="https://www.genusdei.it/wp-content/uploads/2019/04/Coming-Soon.jpg"
+          alt="Coming soon"
+          width="300"
+          height="300"
+        />
     </div>
   );
 };
