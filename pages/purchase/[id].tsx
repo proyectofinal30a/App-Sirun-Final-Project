276c69a847201358188d5c3a59856ee1175b2e36
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-23 16:03:45 +0100

    Avances approved payment

--- pages/purchase/[id].tsx
+++ pages/purchase/[id].tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import HEAD from "../../src-client/components/HEAD";
 import Nav from "../../src-client/components/NavBar/Nav";
 import Footer from "../../src-client/components/Footer/Footer";
+import { getOrder } from "../../src-client/redux/slice/payment/payment";
 import styles from "../../src-client/styles/ApprovedPayment.module.css";
 
 export default function ApprovedPayment() {
+  const { query } = useRouter();
+  const orderId = query.id;
+
+  console.log(query);
+  console.log(orderId);
+
+
+  useEffect(() => {
+    getOrder(orderId);
+  });
+
+
   return (
     <div>
       <HEAD />
--- src-client/redux/slice/payment/payment.ts
+++ src-client/redux/slice/payment/payment.ts
@@ -1,13 +1,71 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import userVerification from "../../../controllers/userVerification-controller";
+// import userVerification from "../../../controllers/userVerification-controller";
 
+
+interface Istate {
+  myOrder: {},
+}
 
 interface ImyOrder {
   myOrder: {
-    
+    external_reference: "", // necesario // nanoid() que recibimos por query
+    id_user: "",            
+    description: "",        
+    total: "", // necesario              
+    status: "", // necesario     
+    date: "", // necesario       
+    delivery_time: "", // necesario     
+    user: Iuser,       
+    product: [],   
+    purchasedProducts: IpurchasedProducts[], // necesario     
+    id_address: "",   
+    address: Iaddress, // necesario     
   },
 };
+
+interface Iuser {
+  user: {
+    id: "",
+    name: "", // necesario
+    email: "", // necesario
+    emailVerified: "",
+    image: "",
+    role: "",
+    accounts: [],
+    addresses: [],
+    evaluations: [],
+    orders: [],
+    sessions: "",
+    favorites: [],
+  }
+}
+
+interface IpurchasedProducts {
+  purchasedPorducts: {
+    title: "", // necesario
+    picture_url: "", // necesario
+    unit_price: number, // necesario
+    quantity: number, // necesario
+    id_order: "",
+    order: ImyOrder,
+  }
+}
+
+interface Iaddress {
+  address: {
+    id: "",
+    id_user: "",
+    name_address: "", 
+    zip_code: number, // necesario
+    street_name: "", // necesario
+    street_number: number, // necesario
+    user: Iuser, 
+    phone: number, // necesario
+    order: ImyOrder,
+  }
+}
+
 
 const initialState: ImyOrder = {
   myOrder: {},
@@ -15,7 +73,7 @@ const initialState: ImyOrder = {
 
 
 export const reducerAfterPayment = createSlice({
-  name: "reducerUser",
+  name: "reducerAfterPayment",
   initialState: initialState,
   reducers: {
     getOrder: (state, action) => {},
@@ -24,7 +82,7 @@ export const reducerAfterPayment = createSlice({
 
 export const getOrder = (id) => async (dispatch: Function) => {
   const { data }: any = await axios.get(`/${id}`);
-  console.log(data)
+  console.log(data);
   dispatch(reducerAfterPayment.actions.getOrder(data));
 };
 
