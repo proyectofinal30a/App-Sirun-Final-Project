6c7465901f168c1129d677a927438dd2129f6010
mailin <mailinadaromdp@gmail.com>
2022-11-22 22:01:57 -0300

    arreglando rutas en pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,7 @@
 import HEAD from "../src-client/components/HEAD";
 import Nav from "../src-client/components/NavBar/Nav";
 import Footer from "../src-client/components/Footer/Footer";
-import ContactUs from "../src-client/components/ContactUs/ContactUs";
+import ContactUs from "../src-client/components/ContactUs/ContactUs"
 import React from "react";
 
 export default function AboutPage() {
