c66ae79f6f98fcc19635f41704df7b0692d26409
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-24 17:11:47 +0100

    Search bar en admin users management

--- src-client/components/Administration/AdminManageUsers.tsx
+++ src-client/components/Administration/AdminManageUsers.tsx
@@ -1,38 +1,71 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
-import { Ireducers, Iuser } from "../../../lib/types";
-import { getAllUsers } from "../../redux/slice/user-detail-redux/all-users";
+import { Ireducers } from "../../../lib/types";
+import {getAllUsers, getUsersByName, clearUserSearch } from "../../redux/slice/user-detail-redux/all-users";
+import cloudinaryOrUrl from "../../controllers/detectionOfImage";
 import styles from "../../styles/AdminManageUsers.module.css";
-
 
 const AdminManageUsers = () => {
   const dispatch: Function = useDispatch();
-  const allUsers: any = useSelector<Ireducers>((state) => state.reducerAllUsers.allUsers); // EZE SEND HELP! :)
 
+  const allUsers = useSelector((state: Ireducers) => state.reducerAllUsers.allUsers);
+  const usersByName = useSelector((state: Ireducers) => state.reducerAllUsers.usersByName);
+
+  const [name, setName] = useState("");
+
+  let currentUsers: any = [];
+  if (usersByName.length > 0) {
+    currentUsers = usersByName;
+  } else {
+    currentUsers = allUsers;
+  }
+
+  
   useEffect(() => {
+    dispatch(clearUserSearch()); 
     dispatch(getAllUsers());
   }, [dispatch]);
-  
-  if (!allUsers) return <div className={styles.users_management__loading}>Loading...</div>
+
+
+  if (!allUsers) return <div className={styles.users_management__loading}>Loading...</div>;
+  console.log(usersByName)
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value.trim());
+    dispatch(getUsersByName(name, allUsers));
+  };
 
 
   return (
     <div className={styles.users_management__container}>
       <h1 className={styles.users_management__title}>Users management</h1>
-      
-      {allUsers[0] ? 
+
+      <div className={styles.users_management__searchbar}>
+        <input
+          type="search"
+          placeholder="Search user name"
+          className={styles.search_bar__input}
+          autoComplete="on"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+      </div>
+
+      {allUsers[0] ? (
         <div className={styles.users__container}>
-          {allUsers.map((user: any, index: number) => {
+          {currentUsers.map((user: any, index: number) => {
             return (
               <div className={styles.user__container} key={user.id}>
                 <div className={styles.user__image_container}>
-                  <Image 
-                    className={styles.user__image} 
-                    src={user.image.split(">")[1]}
+                  <Image
+                    className={styles.user__image}
+                    src={cloudinaryOrUrl(user.image, "client") || ""}
                     alt={"user: " + index}
-                    height="100"
-                    width="100"
+                    height="300"
+                    width="300"
                   />
                 </div>
 
@@ -54,13 +87,13 @@ const AdminManageUsers = () => {
                     {user.role}
                   </p>
                 </div>
-
               </div>
-            )
+            );
           })}
         </div>
-        : <div className={styles.loading}>Loading...</div>
-      }
+      ) : (
+        <div className={styles.loading}>Loading...</div>
+      )}
     </div>
   );
 };
