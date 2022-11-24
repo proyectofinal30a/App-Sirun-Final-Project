05eff48ff0f26c3422f6344b5f3b579f5ccb17e6
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2022-11-24 09:53:46 -0300

    cambio de rama a develop

--- pages/api/userScope/post/order.ts
+++ pages/api/userScope/post/order.ts
@@ -1,9 +1,31 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../../lib/prisma'
+import { IReview } from '../../../../lib/types'
 import { StatusType } from '@prisma/client'
 export default async function Order(req: NextApiRequest, res: NextApiResponse) {
     try {
+        const { idUser, purchasedProducts, address, status, delivery_time, total } = req.body
+
+
+
+//         external_reference String              @id @unique
+//         id_user            String
+//         description        String
+//         total              Int
+//         status             StatusType
+//         date               DateTime            @default(now())
+//         delivery_time      String
+//         user               User                @relation(fields: [id_user], references: [id])
+//         product            Product[]
+//         purchasedProducts  purchasedProducts[]
+//         id_address         String
+//         address            Address   
+// const myDelivery = ''
+
+const myTotal = purchasedProducts.map(elem => elem.unit_price).reducer((e,acc)=>  e + acc)
+
+
         interface id {
             id: string
         }
@@ -16,14 +38,17 @@ export default async function Order(req: NextApiRequest, res: NextApiResponse) {
             delivery_time: string
         }
 
-        const { idUser, idArrayProduct, description, status, delivery_time, total }: order = req.body
 
         const myOrder = await prisma.order.create({
             data: {
-                description,
-                total,
+                external_reference:'s',
+                description:'falta ver esto',
+                total:myTotal,
                 status,
-                delivery_time,
+                delivery_time:'falta ver esto',
+                purchasedProducts:{
+                    createMany:[{title:'d'}]
+                },
                 user: {
                     connect: {
                         id: idUser
@@ -34,6 +59,16 @@ export default async function Order(req: NextApiRequest, res: NextApiResponse) {
                 }
             }
         })
+
+
+
+
+
+
+
+
+
+
         prisma.$disconnect()
         res.status(200).json({ msg: "Order se ha creado" })
 
