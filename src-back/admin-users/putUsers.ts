0a69b81ec7983b2703fdb26c9e7ee8740f2c6748
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-24 20:26:16 +0100

    Avances renderizado users management

--- src-back/admin-users/putUsers.ts
+++ src-back/admin-users/putUsers.ts
@@ -1,14 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
-export const activeUser = async (id:any, status: string) =>{
-    try {
-        const user: any = await axios.put('/api/adminScope/put/updateUser',
-        {
-            id,
-            role: status
-        })
-        return user
-    } catch (error) {
-        return error
-    }
-}
+export const activeUser = async (id: any, status: string) => {
+  try {
+    const user: any = await axios.put("/api/adminScope/put/updateUser", {
+      id,
+      role: status,
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
