59aa2c166d727de8eff8c191d09da41c203cbb12
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2022-11-28 09:46:07 +0100

    mercado pago success route y otras arregladas

--- pages/api/mercadopago/createPayment.ts
+++ pages/api/mercadopago/createPayment.ts
@@ -1,6 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios';
-import { Ipreference, IbodoyPreferenceLink } from '../../../lib/types'
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { Ipreference, IbodoyPreferenceLink } from "../../../lib/types";
+
 export default async function createPayment(req: NextApiRequest, res: NextApiResponse) {
     try {
         const myPreference: Ipreference = req.body
@@ -12,23 +13,26 @@ export default async function createPayment(req: NextApiRequest, res: NextApiRes
                 'Content-Type': "application/json",
                 Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}`
             }
-        })
+        });
 
         const mydata: IbodoyPreferenceLink = {
             order: myPreference,
             purchase_link: response.data.init_point
-        }
-        const myUrl = process.env.NODE_ENV !== 'production' ?
-            'http://localhost:3000/api/userScope/post/order' :
-            'https://sirunnpatisserie.vercel.app/api/userScope/post/order'
+        };
+
+        const myUrl = process.env.NODE_ENV !== 'production' 
+            ? 'http://localhost:3000/api/userScope/post/order'
+            : 'https://sirunnpatisserie.vercel.app/api/userScope/post/order' 
+
         await axios({
             method: "post",
             url: myUrl,
             data: mydata
         })
-        return res.status(200).json({ url: response.data.init_point })
+
+        return res.status(200).json({ url: response.data.init_point });
     } catch (error) {
-        res.status(400).json({ msg: 'Error formProduct' })
+        res.status(400).json({ msg: 'Error formProduct' });
     }
 }
 
